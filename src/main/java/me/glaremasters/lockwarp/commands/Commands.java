43e872aef37b38abd1d1cc0f26d656ea0320ec7b
darbyjack <admin@glaremasters.me>
2018-10-04 10:07:44 -0500

    Added in perm check and permission node custom

--- src/main/java/me/glaremasters/lockwarp/commands/Commands.java
+++ src/main/java/me/glaremasters/lockwarp/commands/Commands.java
@@ -5,7 +5,13 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.CommandHelp;
 import co.aikar.commands.annotation.*;
 import me.glaremasters.lockwarp.LockWarp;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static co.aikar.commands.ACFBukkitUtil.color;
 
 /**
  * Created by GlareMasters
@@ -17,12 +23,19 @@ public class Commands extends BaseCommand {
 
     @Dependency private LockWarp lockWarp;
 
+    private Set<String> coolDown = new HashSet<>();
+
     @Subcommand("setwarp")
     @CommandPermission("lw.setwarp")
-    @Syntax("<warp name>")
-    public void onSetWarp(Player player, String warpName) {
-        lockWarp.getWarpsConfig().set(warpName, ACFBukkitUtil.fullLocationToString(player.getLocation()));
+    @Syntax("<warp name> <cooldown> <require permission (true / false)> <permission node>")
+    public void onSetWarp(Player player, String warpName, Integer cooldown, boolean requirePermission, @Optional String permissionNode) {
+        FileConfiguration warpsConfig = lockWarp.getWarpsConfig();
+        warpsConfig.set(warpName + ".loc", ACFBukkitUtil.fullLocationToString(player.getLocation()));
+        warpsConfig.set(warpName + ".cd", cooldown);
+        warpsConfig.set(warpName + ".checkPerm", requirePermission);
+        warpsConfig.set(warpName + ".permNode", permissionNode);
         lockWarp.saveData();
+        player.sendMessage(color(lockWarp.getConfig().getString("messages.setwarp")).replace("{warp}", warpName).replace("{time}", String.valueOf(cooldown)));
     }
 
 
